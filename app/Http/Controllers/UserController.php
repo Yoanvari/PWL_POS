a77f5c90e2978ea962c46fd12ad46b64858d921c
Yoanvari <yoanvari123@gmail.com>
2024-03-11 09:53:24 +0700

    Praktikum 2.2 Jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,9 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(4004);
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
