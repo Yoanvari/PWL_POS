b08efa95d9daf61bbc719e9dd85ee1c2fafaca83
Yoanvari <yoanvari123@gmail.com>
2024-03-11 16:33:28 +0700

    Praktimuk 2.6 Jobsheet 4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,29 +8,24 @@
 </head>
 <body>
     <h1>Data User</h1>
+    <a href="/PWL_POS/public/user/tambah">+ Tambah User</a>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
+            <th>Aksi</th>
         </tr>
-        {{-- @foreach ($data as $d) --}}
+        @foreach ($data as $d)
             <tr>
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_Id }}</td>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_Id }}</td>
+                <td><a href="/PWL_POS/public/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/PWL_POS/public/user/hapus/{{ $d->user_id }}">Hapus</a></td>
             </tr>
-        {{-- @endforeach --}}
+        @endforeach
     </table>
-    {{-- <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>Jumlah Pengguna</th>
-        </tr>
-        <tr>
-            <td>{{ $data }}</td>
-        </tr>
-    </table> --}}
 </body>
 </html>
